9ca7ec192818cc7dc44cee7e8d452285875eab44
Victor Lam <victorebox@yahoo.com.hk>
2014-10-27 16:58:53 +0800

    init gem

---
+++ lib/onesky/rails.rb
@@ -0,0 +1,7 @@
+require "onesky/rails/version"
+
+module Onesky
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/onesky/rails/version.rb
@@ -0,0 +1,5 @@
+module Onesky
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
