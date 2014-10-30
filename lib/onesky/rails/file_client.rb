58150bbb7429b8b69d7c426a97d27439d71403a8
Victor Lam <victorebox@yahoo.com.hk>
2014-10-30 18:05:49 +0800

    implement file_client

---
+++ lib/onesky/rails/file_client.rb
@@ -0,0 +1,33 @@
+require 'onesky/rails/client'
+require 'yaml'
+
+module Onesky
+  module Rails
+
+    class FileClient < Onesky::Rails::Client
+
+      FILE_FORMAT = 'RUBY_YAML'
+
+      def upload(string_path)
+        path_prefix = ::Rails.root.join("config/locales/").to_s
+        get_default_locale_files(string_path).map do |path|
+          filename = path.sub(path_prefix, '')
+          puts "Uploading #{filename}"
+          @project.upload_file(file: path, file_format: FILE_FORMAT)
+          path
+        end
+      end
+
+      protected
+
+      def get_default_locale_files(string_path)
+        Dir.glob("#{string_path}/**/*.yml").map do |path|
+          content_hash = YAML.load_file(path)
+          path if content_hash.has_key?(@base_locale.to_s)
+        end.compact
+      end
+
+    end
+
+  end
+end
