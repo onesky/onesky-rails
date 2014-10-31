a4619733e5f603b241e5e825c2ca6f89109d2c8e
Victor Lam <victorebox@yahoo.com.hk>
2014-10-31 16:37:01 +0800

    download translations

--- lib/onesky/rails/file_client.rb
+++ lib/onesky/rails/file_client.rb
@@ -7,17 +7,56 @@ module Onesky
     class FileClient < Onesky::Rails::Client
 
       FILE_FORMAT = 'RUBY_YAML'
+      ENCODING    = 'UTF-8'
+      DIR_PREFIX  = 'onesky_'
 
       def upload(string_path)
         get_default_locale_files(string_path).map do |path|
-          filename = path.sub(string_path, '')
+          filename = File.basename(path)
           puts "Uploading #{filename}"
           @project.upload_file(file: path, file_format: FILE_FORMAT)
           path
         end
       end
 
+      def download(string_path)
+        files = get_default_locale_files(string_path).map {|path| File.basename(path)}
+
+        @onesky_locales.each do |locale|
+          puts "#{locale_dir(locale)}/"
+          onesky_locale = locale.gsub('_', '-')
+          files.each do |file|
+            translations = @project.export_translation(source_file_name: file, locale: onesky_locale)
+            locale_path = make_translation_dir(string_path, locale)
+            target_file = extract_file_name(translations.headers) || file
+            File.open(File.join(locale_path, target_file), 'w') do |f|
+              f.write(translations.force_encoding(ENCODING))
+            end
+            puts "  #{target_file}"
+          end
+        end
+      end
+
       protected
+
+      def locale_dir(locale)
+        DIR_PREFIX + locale
+      end
+
+      def extract_file_name(header_hash)
+        search_text = 'filename='
+        if disposition = header_hash[:content_disposition]
+          if idx = disposition.index(search_text)
+            disposition[(idx + search_text.length)..-1]
+          end
+        end
+      end
+
+      def make_translation_dir(dir_path, locale)
+        target_path = File.join(dir_path, locale_dir(locale))
+        Dir.mkdir(target_path) unless File.directory?(target_path)
+        target_path
+      end
 
       def get_default_locale_files(string_path)
         Dir.glob("#{string_path}/**/*.yml").map do |path|
--- spec/onesky/rails/file_client_spec.rb
+++ spec/onesky/rails/file_client_spec.rb
@@ -17,12 +17,55 @@ describe Onesky::Rails::FileClient do
       stub_request(:post, full_path_with_auth_hash("/projects/#{project_id}/files", api_key, api_secret))
         .to_return(status: 201)
 
-      
       expect(client.upload(file_path)).to eq(["#{file_path}/en.yml"])
     end
   end
 
   context 'download' do
+
+    let(:file_name) {'ja.yml'}
+    let(:response_headers) do
+      {
+        'Content-Type'        => 'text/plain',
+        'Content-Disposition' => "attachment; filename=#{file_name}",
+      }
+    end
+    let(:query_string) {'&source_file_name=en.yml&locale=ja'}
+
+    def locale_dir
+      File.join(file_path, 'onesky_ja')
+    end
+
+    def locale_files
+      Dir.glob("#{locale_dir}/*.yml")
+    end
+
+    def delete_test_dir
+      if File.directory?(locale_dir)
+        File.delete(*Dir.glob("#{locale_dir}/**/*"))
+        Dir.delete(locale_dir)
+      end
+    end
+
+    before(:each) do
+      delete_test_dir
+    end
+
+    after(:each) do
+      delete_test_dir
+    end
+
+    it 'download translations from OneSky and save as YAML files' do
+      stub_request(:get, full_path_with_auth_hash("/projects/#{project_id}/translations", api_key, api_secret) + query_string)
+        .to_return(
+          status: 200,
+          body: File.read(File.join(file_path, file_name)),
+          headers: response_headers)
+
+      expect(locale_files).to be_empty
+      client.download(file_path)
+      expect(locale_files).to eq(["#{locale_dir}/#{file_name}"])
+    end
   end
 
 end
